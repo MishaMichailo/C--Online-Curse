b43b7e7d39956421d7339182c3cea5b1f900b9e6
MishaMichailo <119402999+MishaMichailo@users.noreply.github.com>
2023-05-17 18:34:20 +0300

    Exeption

---
+++ Exeption2_HomeWork/Program.cs
@@ -0,0 +1,56 @@
+﻿namespace Exeption2_HomeWork
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter start: ");
+            int start = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter end: ");
+            int end = int.Parse(Console.ReadLine());
+
+            int[] number = new int[10];
+
+            for (int i = 0; i < number.Length;i++)
+            {
+                try
+                {
+                    number[i++] = ReadNumber(start, end);
+                    start = number[i-1];
+
+                }
+
+
+
+
+                catch (ArgumentException ex)
+                {
+                    Console.WriteLine("The num should be greater than previous number entered:" + ex.Message);
+                    i--;
+                }
+
+
+            }
+            Console.WriteLine("You have entered the following numbers: ");
+            Console.WriteLine(String.Join("\t", number));
+        }
+      private  static int ReadNumber(int start, int end)
+        {
+            Console.WriteLine("Enter a number");
+            int number = int.Parse(Console.ReadLine());
+            Console.ForegroundColor = ConsoleColor.Red;
+            if (number < start || number > end)
+            {
+                throw new ArgumentException($"The number should be between {start} and {end}.");
+            }
+            Console.ResetColor();
+
+            return number;
+        }
+    }
+}
+
+
+            
+        
+
---
+++ Exeption_HomeWork/Program.cs
@@ -0,0 +1,52 @@
+﻿namespace Exeption_HomeWork
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Enter first num: ");
+                int num1 = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Enter second num : ");
+                int num2 = Convert.ToInt32(Console.ReadLine());
+
+
+                double res = Divide(num1, num2);
+                Console.WriteLine($"Result of division: {res}");
+
+                if (num1 == ',' && num2 == ',')
+                {
+                    throw new Exception("Throwing exception if dividing of two double numbers");
+                }
+            }
+            catch (DivideByZeroException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Throwing exception if dividing of two double numbers: {ex.Message}");
+            }
+
+
+
+        }
+        static double Divide(double num1, double num2)
+        {
+            if (num1 == 0.0 || num2 == 0.0)
+            {
+                throw new DivideByZeroException("dividing of two double numbers not allowed");
+            }
+
+            return num1 / num2;
+        }
+
+    }
+
+    
+}
+      
+        
+    
